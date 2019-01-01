182e117bedaf2ace36e9d27b63f69995e8b9d921
Joseegudarrama <Guadarramaje@gmail.com>
2019-01-01 12:52:04 -0600

    Author update funct. added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -148,11 +148,68 @@ exports.author_delete_post = function(req, res, next) {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = function(req, res, next) {
+    
+    Author.findById(req.params.id)
+        .exec( function(err, author){
+            if (err) { return next(err); }
+            if (author == null) { 
+                // No results.
+                var error = new Error('Genre not found');
+                error.status = 404;
+                return next(error);
+            }
+            res.render('author_form', {title: 'Update Author', author: author});
+        });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+    
+    //Validate fields
+    body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1 }).trim().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+    //Sanatize fields.
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').trim().escape(),
+    sanitizeBody('date_of_death').trim().escape(),
+    
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+            // Create an Author object with escaped and trimmed data.
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death,
+                    _id: req.body.id
+                });
+            Author.findByIdAndUpdate(req.params.id, author, {}, function (err, theauthor){
+                if (err) { return next(err); }
+                
+                //Succesful - redirect to updated book detail page.
+                res.redirect(theauthor.url);
+            });
+        }
+    }
+
+
+];
